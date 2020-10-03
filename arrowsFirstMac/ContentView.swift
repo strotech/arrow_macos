2e494a552f9c8bf4948f7ecdc474df9537191cde
Sijil Salim <sijils@gmail.com>
2020-10-03 12:36:56 +0530

    Initial Commit

---
+++ arrowsFirstMac/ContentView.swift
@@ -0,0 +1,17 @@
+//Created by me on 03/10/20
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
